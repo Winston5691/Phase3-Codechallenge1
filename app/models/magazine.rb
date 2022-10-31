2e454b22fac043dfdb935f4db0e227ef8ad72848
Winston Langat <winston.langat@student.moringaschool.com>
2022-10-31 06:13:12 +0300

    Magazine

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,58 +1,37 @@
+require_relative "article"
+
 class Magazine
   attr_accessor :name, :category
-  @@all = []
+  @@all=[]
+
 
   def initialize(name, category)
     @name = name
     @category = category
     @@all << self
   end
-
   def self.all
     @@all
   end
-
   def articles
-    Article.all.select { |article| article.magazine == self }
-  end
+    Article.all.select{|article|article.magazine=self}
 
-  def authors
-    articles.collect { |article| article.author }.uniq
   end
-
   def article_titles
-    articles.collect { |article| article.title }
+    articles.map{|aricle| article.Articles.title}
   end
 
-  def author_names
-    authors.collect { |author| author.name }
   end
-
-  def contributor_names
-    Article.all.filter { |article| article.magazine == self }.map { |article| article.author.name }.uniq
+  def contributors
+    articles.map{|article|article.author}
   end
+  def find_by_name
 
-  def self.find_by_name(name)
-    self.all.find { |magazine| magazine.name == name }
   end
-
-  def self.find_by_category(category)
-    self.all.select { |magazine| magazine.category == category }
-  end
-
-  def self.most_popular
-    self.all.max_by { |magazine| magazine.contributor_names.count }
-  end
-
-  def contributor_count
-    contributor_names.count
-  end
-
   def contributing_authors
-    popular_authors = Article.all.filter { |article| article.magazine.name == @name }.map {
-      |article|
-      article.author.name
-    }.tally.each { |key, value| value > 2 }
-    popular_authors
+    @@all.select{|article|article.contributors>2}
   end
+
+
+
 end
