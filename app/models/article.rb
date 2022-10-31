d421158df98851c5b2e9674531f8a993c550e422
Winston Langat <winston.langat@student.moringaschool.com>
2022-10-31 06:18:21 +0300

    Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,36 +1,19 @@
 class Article
-    @@all = []
+  attr_reader :title, :author, :magazine
+  @@all=[]
+
+  def initialize(title,author,magazine)
+    @title=title
+    @author=author
+    @magazine=magazine
+    @@all << self
   
-    attr_accessor :title, :magazine
-  
-    def initialize(title, magazine, author)
-      @title = title
-      @magazine = magazine
-      @author = author
-      @@all << self
-    end
-  
-    def self.all
-      @@all
-    end
-  
-    def author
-      @author
-    end
-  
-    def magazine
-      @magazine
-    end
-  
-    def title
-      @title
-    end
-  
-    def author_name
-      self.author.name
-    end
-  
-    def magazine_name
-      self.magazine.name
-    end
   end
+  def self.all
+    @all
+  end
+
+  
+    
+
+end
